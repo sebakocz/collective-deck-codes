ac9e80fc93602723861a5437c13c48cebfaaf95b
Sebastian Koczwara <sebakocz@gmail.com>
2022-07-24 17:44:47 +0200

    dynamic deck meta

--- src/components/common/carddisplay.tsx
+++ src/components/common/carddisplay.tsx
@@ -64,6 +64,7 @@ export default function CardDisplay({dc, tooltipOffset}: CardDisplayProps) {
                         rel={"noreferrer"}
                         data-tip={dc.card.link}
                         data-for={"card-tooltip"}
+                        onClick={e => e.stopPropagation()}
                     >
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
                             <path strokeLinecap="round" strokeLinejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
--- src/pages/decks/[id].tsx
+++ src/pages/decks/[id].tsx
@@ -13,6 +13,8 @@ import Button from "../../components/common/button";
 import {exportDeckToClipboard, noHero} from "../../lib/utils";
 import {Affinity, Type} from "@prisma/client";
 import {Tooltip} from "react-tippy";
+import cubeImg from "*.png";
+import Head from "next/head";
 
 // // https://trpc.io/docs/ssg
 // export async function getStaticProps(
@@ -127,76 +129,84 @@ const DeckProfile = ( props: InferGetStaticPropsType<typeof getStaticProps>) => 
     const {deck}:{deck: Deck} = props
 
     return(
-        <div className={"flex flex-col justify-start items-center w-full p-5 gap-5"}>
+        <>
+            <Head>
+                <title>Collective Deck - {deck.name} - by {deck.user.name}</title>
+                <meta name="description" content={deck.description || "No Description."} />
+                <meta property="og:image" content={`https://www.collective.gg/emotes/${slugify(deck?.hero?.name || "", {replacement: '', lower: true})}_thumb.png`}/>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
 
-            {/* Header */}
-            <div className={"min-w-[300px] h-40 flex gap-2"}>
+            <div className={"flex flex-col justify-start items-center w-full p-5 gap-5"}>
 
-                {/* Hero Icon */}
-                <div className={"w-40 h-40 circle"}>
-                    <Image src={`https://www.collective.gg/emotes/${slugify(deck?.hero?.name || "", {replacement: '', lower: true})}_thumb.png`}
-                           width={250}
-                           height={250}
-                           alt={deck.hero?.name}
-                        // objectFit={"fill"}
-                        // layout={"fill"}
-                    />
+                {/* Header */}
+                <div className={"min-w-[300px] h-40 flex gap-2"}>
+
+                    {/* Hero Icon */}
+                    <div className={"w-40 h-40 circle"}>
+                        <Image src={`https://www.collective.gg/emotes/${slugify(deck?.hero?.name || "", {replacement: '', lower: true})}_thumb.png`}
+                               width={250}
+                               height={250}
+                               alt={deck.hero?.name}
+                            // objectFit={"fill"}
+                            // layout={"fill"}
+                        />
+                    </div>
+
+                    {/* Deck Info */}
+                    <div className={"grow p-2"}>
+                        <div className={"text-2xl font-bold"}>{deck.name}</div>
+                        <div className={"text-sm"}>by {deck.user.name}</div>
+
+                        <p className={"italic mt-6"}>
+                            {deck.description || "No description"}
+                        </p>
+                    </div>
                 </div>
 
-                {/* Deck Info */}
-                <div className={"grow p-2"}>
-                    <div className={"text-2xl font-bold"}>{deck.name}</div>
-                    <div className={"text-sm"}>by {deck.user.name}</div>
+                {/* Interaction Section */}
+                <div className={"flex justify-between gap-6"}>
 
-                    <p className={"italic mt-6"}>
-                        {deck.description || "No description"}
-                    </p>
-                </div>
-            </div>
+                    {/* TODO: OnClick */}
+                    {/* @ts-ignore */}
+                    <Tooltip
+                        position={"bottom"}
+                        title={"Under Construction. Blame Sevas or something."}
+                    >
+                        <Button disabled={true}>
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M19.428 15.428a2 2 0 00-1.022-.547l-2.387-.477a6 6 0 00-3.86.517l-.318.158a6 6 0 01-3.86.517L6.05 15.21a2 2 0 00-1.806.547M8 4h8l-1 1v5.172a2 2 0 00.586 1.414l5 5c1.26 1.26.367 3.414-1.415 3.414H4.828c-1.782 0-2.674-2.154-1.414-3.414l5-5A2 2 0 009 10.172V5L8 4z" />
+                            </svg>
+                            Brew
+                        </Button>
+                    </Tooltip>
 
-            {/* Interaction Section */}
-            <div className={"flex justify-between gap-6"}>
-
-                {/* TODO: OnClick */}
-                {/* @ts-ignore */}
-                <Tooltip
-                    position={"bottom"}
-                    title={"Under Construction. Blame Sevas or something."}
-                >
-                    <Button disabled={true}>
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
-                            <path strokeLinecap="round" strokeLinejoin="round" d="M19.428 15.428a2 2 0 00-1.022-.547l-2.387-.477a6 6 0 00-3.86.517l-.318.158a6 6 0 01-3.86.517L6.05 15.21a2 2 0 00-1.806.547M8 4h8l-1 1v5.172a2 2 0 00.586 1.414l5 5c1.26 1.26.367 3.414-1.415 3.414H4.828c-1.782 0-2.674-2.154-1.414-3.414l5-5A2 2 0 009 10.172V5L8 4z" />
+                    <Button onClick={() => exportDeckToClipboard(deck.name, deck.cards, deck.hero || noHero)}>
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-8l-4-4m0 0L8 8m4-4v12" />
                         </svg>
-                        Brew
+                        Export
                     </Button>
-                </Tooltip>
+                </div>
 
-                <Button onClick={() => exportDeckToClipboard(deck.name, deck.cards, deck.hero || noHero)}>
-                    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
-                        <path strokeLinecap="round" strokeLinejoin="round" d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-8l-4-4m0 0L8 8m4-4v12" />
-                    </svg>
-                    Export
-                </Button>
+                {/* Deck */}
+                <div className={"min-w-[300px] w-2/5 flex justify-between"}>
+
+                    {/* Units */}
+                    <TypeList
+                        deck={deck}
+                        type={Type.UNIT}
+                    />
+
+                    {/* Action */}
+                    <TypeList
+                        deck={deck}
+                        type={Type.ACTION}
+                    />
+
+                </div>
             </div>
-
-            {/* Deck */}
-            <div className={"min-w-[300px] w-2/5 flex justify-between"}>
-
-                {/* Units */}
-                <TypeList
-                    deck={deck}
-                    type={Type.UNIT}
-                />
-
-                {/* Action */}
-                <TypeList
-                    deck={deck}
-                    type={Type.ACTION}
-                />
-
-            </div>
-
-        </div>
+        </>
     )
 }
 
