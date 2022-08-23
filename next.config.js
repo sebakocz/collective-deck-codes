3f4b4d7ceddfe802fc81546816eeccb6d9e504c3
Sebastian Koczwara <sebakocz@gmail.com>
2022-08-23 17:51:39 +0200

    compile fixes

--- next.config.js
+++ next.config.js
@@ -2,8 +2,13 @@
 const nextConfig = {
   reactStrictMode: false,
   images: {
-    domains: ['s3.us-east-2.amazonaws.com', 'www.collective.gg', 'cdn.discordapp.com', 'files.collective.gg'],
+    domains: ['s3.us-east-2.amazonaws.com', 'www.collective.gg', 'cdn.discordapp.com', 'files.collective.gg']
   }
 }
 
 module.exports = nextConfig
+
+// next.config.js
+const withTM = require('next-transpile-modules')(['next-goatcounter']); // pass the modules you would like to see transpiled
+
+module.exports = withTM({});
