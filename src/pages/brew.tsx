ba7c326890fb22a0958594986adf6f608f9f65ab
Sebastian Koczwara <sebakocz@gmail.com>
2022-09-08 11:18:34 +0200

    no hero: include exlusives by default

--- src/pages/brew.tsx
+++ src/pages/brew.tsx
@@ -100,9 +100,11 @@ const Brew: NextPage = () => {
         })
         setDeck(sortCards([...newDeckCards]))
 
+        console.log(hero.name)
+
         // when a non-non-hero is selected show exclusive cards
         // TODO: error: select hero -> refresh filters, you should change the default filter instead hacking like this
-        if(hero.name.includes("No Hero") && !miscFilterExists(exclusiveFilter)){
+        if(hero.name.includes("No Hero") && miscFilterExists(exclusiveFilter)){
             toggleMiscFilter(exclusiveFilter)
         }
         if(!hero.name.includes("No Hero") && miscFilterExists(exclusiveFilter)){
