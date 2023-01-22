cf598bdf3aa823fd37d46194cbd7ff2122d04a57
Sebastian Koczwara <sebakocz@gmail.com>
2023-01-22 22:58:25 +0100

    toolTip fixes

--- src/components/common/toolTipProvider.tsx
+++ src/components/common/toolTipProvider.tsx
@@ -1,41 +1,44 @@
-import ReactTooltip from "react-tooltip";
+import collectiveIcon from "@public/collective_icon.png";
 import Image from "next/image";
-import parrotImg from "../../../public/Parroting_Parrot.png";
-import collectiveIcon from "../../../public/collective_icon.png";
-import {Tooltip} from "react-tippy";
+import { Tooltip } from "react-tippy";
 
 type ToolTipProviderProps = {
-    children: any,
-    link: string,
-    tooltipOffset?: number,
-}
+  children: any;
+  link: string;
+  tooltipOffset?: number;
+};
 
-const ToolTipProvider = ({children, link, tooltipOffset}: ToolTipProviderProps) => {
-    return (
-        // @ts-ignore - https://github.com/tvkhoa/react-tippy/issues/169
-        <Tooltip
-            arrow={false}
-            animateFill={false}
-            theme={'light'}
-            position={'right'}
-            distance={tooltipOffset || 10}
-            html={
-                <div className={"w-2/3 md:w-96"}>
-                    <Image
-                        src={link}
-                        alt={"Card Full Image"}
-                        width={450}
-                        height={450}
-                        objectFit={"contain"}
-                        placeholder={'blur'}
-                        blurDataURL={collectiveIcon.src}
-                    />
-                </div>
-            }
-        >
-            {children}
-        </Tooltip>
-    )
-}
+const ToolTipProvider = ({
+  children,
+  link,
+  tooltipOffset,
+}: ToolTipProviderProps) => {
+  return (
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore - https://github.com/tvkhoa/react-tippy/issues/169
+    <Tooltip
+      arrow={false}
+      animateFill={false}
+      theme={"light"}
+      position={"right"}
+      distance={tooltipOffset || 10}
+      html={
+        <div className={"w-2/3 md:w-96"}>
+          <Image
+            src={link}
+            alt={"Card Full Image"}
+            width={450}
+            height={450}
+            objectFit={"contain"}
+            placeholder={"blur"}
+            blurDataURL={collectiveIcon.src}
+          />
+        </div>
+      }
+    >
+      {children}
+    </Tooltip>
+  );
+};
 
 export default ToolTipProvider;
