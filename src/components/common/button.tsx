327324b8fade6ca603e4936ae33f5b2b711358f6
Sebastian Koczwara <sebakocz@gmail.com>
2023-01-22 23:01:04 +0100

    button fixes

--- src/components/common/button.tsx
+++ src/components/common/button.tsx
@@ -1,20 +1,22 @@
 type ButtonProps = {
-    onClick?: () => void,
-    children: any,
-    disabled?: boolean
-    moreClasses?: string
-}
+  onClick?: () => void;
+  children: any;
+  disabled?: boolean;
+  moreClasses?: string;
+};
 
-const Button = ({children, onClick, disabled, moreClasses}: ButtonProps) => {
-    return (
-        <button
-            className={`min-w-[150px] max-h-12 p-2 rounded text-center bg-main-300 enabled:hover:bg-main-200 duration-300 text-lg flex justify-center items-center gap-1 shadow ${disabled ? "opacity-50" : ""} ${moreClasses}`}
-            onClick={onClick}
-            disabled={disabled}
-        >
-            {children}
-        </button>
-    );
-}
+const Button = ({ children, onClick, disabled, moreClasses }: ButtonProps) => {
+  return (
+    <button
+      className={`flex max-h-12 min-w-[150px] items-center justify-center gap-1 rounded bg-main-300 p-2 text-center text-lg shadow duration-300 enabled:hover:bg-main-200 ${
+        disabled ? "opacity-50" : ""
+      } ${moreClasses || ""}`}
+      onClick={onClick}
+      disabled={disabled}
+    >
+      {children}
+    </button>
+  );
+};
 
 export default Button;
